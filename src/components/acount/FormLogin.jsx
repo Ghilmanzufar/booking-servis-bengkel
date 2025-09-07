58c942c532171afd06002a0fa8a4667e85dc2647
Ghilmanzufar <ghilmanzufar2004@gmail.com>
2025-09-07 19:04:05 +0700

    merubah formREGISTER dan login -PC

--- src/components/acount/FormLogin.jsx
+++ src/components/acount/FormLogin.jsx
@@ -41,7 +41,7 @@ const LoginForm = () => {
 
     return (
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 backdrop-blur-sm">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Login Ke Akun Anda
